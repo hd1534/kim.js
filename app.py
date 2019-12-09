00eff774312c1f988d6efeca912c6e9539a16a68
nahjh2 <tkfkadls123>
2019-12-10 08:29:19 +0900

    fix circular import dependencies

--- app.py
+++ app.py
@@ -1,16 +1,29 @@
 from flask import Flask
-from api import api
+from flask_migrate import Migrate
+from apis import api
+from setting import debug
 from flask_cors import CORS
+from databases import DB
+from setting import (
+    DB_USER,
+    DB_PASSWORD,
+    DB_ADDRESS,
+    DB_NAME
+)
 
 app = Flask(__name__)
 api.init_app(app)
 app.url_map.strict_slashes = False
 app.config['ERROR_INCLUDE_MESSAGE'] = False
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 30  # 캐시 컨트롤 시간을 30초로
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'mysql+pymysql://{}:{}@{}/{}?charset=utf8mb4'\
+    .format(DB_USER, DB_PASSWORD, DB_ADDRESS, DB_NAME)
 
 CORS(app)
-
+DB.init_app(app)
+migrate = Migrate(app, DB)
 
 if __name__ == "__main__":
-    __import__('databases')
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=debug, host='127.0.0.1', port=5000)
--- databases/__init__.py
+++ databases/__init__.py
@@ -1,21 +1,4 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from app import app
-from setting import (
-    DB_USER,
-    DB_PASSWORD,
-    DB_ADDRESS,
-    DB_NAME
-)
 
+DB = SQLAlchemy()
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] =\
-    'mysql+pymysql://{}:{}@{}/{}?charset=utf8mb4'\
-    .format(DB_USER, DB_PASSWORD, DB_ADDRESS, DB_NAME)
-
-DB = SQLAlchemy(app)
-migrate = Migrate(app, DB)
-
-
-__import__('databases.user')
